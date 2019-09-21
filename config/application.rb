3a868e01e4dab9fc9b5e05fbfb6ee9d16a516770
ishida_masakazu <u225210b@gmail.com>
2019-09-21 16:25:20 +0900

    add rspec

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,14 @@ module FreeAd
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.controller decorator: false
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework :rspec, view_specs: false, routing_specs: false, fixture: true
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
   end
 end
