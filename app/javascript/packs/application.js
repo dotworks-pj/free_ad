f306d5f8e4ef557b774b626125325e194bf1ba2e
ishida_masakazu <u225210b@gmail.com>
2019-10-01 00:10:44 +0900

    add design template

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,12 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
+//= require ../application/jquery.min
+//= require ../application/bootstrap.min
+//= require_tree ../application/plugins
+//= require ../application/shards.min
+//= require_tree ../application
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
