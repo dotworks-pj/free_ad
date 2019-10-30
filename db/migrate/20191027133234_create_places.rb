172e1619a1264ad4bbe5556523bfb9a96a737670
ishida_masakazu <u225210b@gmail.com>
2019-10-31 00:22:04 +0900

    add place model

---
+++ db/migrate/20191027133234_create_places.rb
@@ -0,0 +1,16 @@
+class CreatePlaces < ActiveRecord::Migration[6.0]
+  def change
+    create_table :places do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :title, null: false
+      t.text :description, null: false
+      t.string :name, null: false
+      t.string :address, null: false
+      t.string :station, null: false
+      t.text :url
+      t.string :maim_image, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_20_053709) do
+ActiveRecord::Schema.define(version: 2019_10_27_133234) do
 
   create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,20 @@ ActiveRecord::Schema.define(version: 2019_10_20_053709) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "places", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title", null: false
+    t.text "description", null: false
+    t.string "name", null: false
+    t.string "address", null: false
+    t.string "station", null: false
+    t.text "url"
+    t.string "maim_image", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_places_on_user_id"
   end
 
   create_table "space_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -67,6 +81,7 @@ ActiveRecord::Schema.define(version: 2019_10_20_053709) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "places", "users"
   add_foreign_key "space_images", "spaces"
   add_foreign_key "spaces", "users"
 end
