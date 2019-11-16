c1b3769ad0b3eda694850bcc1af84237ea9e4a22
ishida_masakazu <u225210b@gmail.com>
2019-11-17 00:22:15 +0900

    test

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -3,5 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe AdminUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it '' do
+    expect(admin_user).to eq 'a'
+  end
 end
